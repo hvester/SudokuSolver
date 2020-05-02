884a78076d7fd932cb609645eb7a04c163cd4d21
Heikki Vesterinen <hvesteri@gmail.com>
2020-05-02 12:56:24 +0300

    Coloring by backtracking algorithm

--- SudokuSolver/Graph.fs
+++ SudokuSolver/Graph.fs
@@ -14,6 +14,8 @@ type UndirectedAdjacencyMatrix(nVertices) =
         let mask = 1uy <<< (logicalIndex - 8 * arrayIndex)
         arrayIndex, mask
 
+    member __.Vertices = [ 0 .. nVertices - 1 ]
+
     member __.AddEdge(i, j) =
         let arrayIndex, mask = getArrayIndexAndMask i j
         let currentValue = adjacencies.[arrayIndex]
@@ -27,3 +29,57 @@ type UndirectedAdjacencyMatrix(nVertices) =
     member __.HasEdge(i, j) =
         let arrayIndex, mask = getArrayIndexAndMask i j
         adjacencies.[arrayIndex] &&& mask <> 0uy
+
+    member this.GetAdjacentVertices(i) =
+        [ for j in 0 .. nVertices - 1 do
+            if this.HasEdge(i, j) then
+                yield j ]
+
+
+
+module Coloring =
+    open System.Collections.Generic
+
+    let runColoring (colors : 'Color array) initialColors (graph : UndirectedAdjacencyMatrix) =
+        let state = Dictionary<_,_>()
+        initialColors |> List.iter state.Add
+        let vertices = List.toArray graph.Vertices
+
+        let canSetColor vertex color =
+            if state.ContainsKey vertex then
+                false
+            else
+                graph.GetAdjacentVertices(vertex)
+                |> List.forall (fun adjacentVertex ->
+                    match state.TryGetValue adjacentVertex with
+                    | false, _  -> true
+                    | true, adjacentColor ->
+                        color <> adjacentColor)
+
+        let moveIndices (vertexIndex, colorIndex) =
+            let newColorIndex = (colorIndex + 1) % colors.Length
+            let newVertexIndex = if newColorIndex = 0 then vertexIndex + 1 else vertexIndex
+            (newVertexIndex, newColorIndex) 
+
+        let rec loop stack =
+            match stack with
+            | [] -> None
+            | ((vertexIndex, colorIndex), prevVertex) :: remainingStack ->
+                if vertexIndex >= vertices.Length then
+                    prevVertex |> Option.iter (state.Remove >> ignore)
+                    loop remainingStack
+                else
+                    let nextVertex = vertices.[vertexIndex]
+                    let nextColor = colors.[colorIndex]
+                    let updatedStack = (moveIndices (vertexIndex, colorIndex), prevVertex) :: remainingStack
+                    if canSetColor nextVertex nextColor then
+                        state.Add(nextVertex, nextColor)
+                        if state.Count = vertices.Length then
+                            [ for vertex in vertices do yield (vertex, state.[vertex]) ]
+                            |> Some
+                        else
+                            loop (((0, 0), Some nextVertex) :: updatedStack)
+                    else
+                        loop updatedStack
+
+        loop [ ((0, 0), None) ]
