99716b5d3d9e1e99fee9b7cdf9661877de85d3ff
Heikki Vesterinen <hvesteri@gmail.com>
2020-05-06 21:25:53 +0300

    Sudoku parsing and formatting

--- SudokuSolver/Program.fs
+++ SudokuSolver/Program.fs
@@ -1,10 +1,47 @@
 ﻿namespace SudokuSolver
 
 open System
+open System.IO
+open System.Diagnostics
+open Sudoku
 
 module Main =
 
-    
+    let tryParseRow (str : string) =
+        let squares =
+            str.ToCharArray()
+            |> Array.choose (fun c ->
+                match Int32.TryParse(string c) with
+                | true, n when n > 0 -> Some (Some n)
+                | _ when c = ' ' -> Some None  
+                | _ -> None)
+        if squares.Length >= 9 then
+            Some (Array.take 9 squares)
+        else
+            None
+
+
+    let parseSudoku (str : string) =
+        let rows =
+            str.Split("\n")
+            |> Array.choose tryParseRow
+        if rows.Length = 9 then
+            rows
+        else
+            failwith "Invalid input: There should be nine lines with nine numbers or spaces"
+
+
+    let formatSolvedSudoku (sudoku : int array array) =
+        sudoku
+        |> Array.map (fun row ->
+            Array.map string row
+            |> Array.chunkBySize 3
+            |> Array.map (String.concat "")
+            |> String.concat "|")
+        |> Array.chunkBySize 3
+        |> Array.map (String.concat "\n")
+        |> String.concat "\n---+---+---\n"
+
 
     [<EntryPoint>]
     let main argv =
